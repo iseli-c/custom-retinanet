15f679eb6b156af975db81b894858713c36c524e
iseli-c <48895161+iseli-c@users.noreply.github.com>
2019-03-25 11:27:06 +1100

    Update build.sh

--- lib/build.sh
+++ lib/build.sh
@@ -5,7 +5,7 @@ CUDA_ARCH="-gencode arch=compute_30,code=sm_30 \
            -gencode arch=compute_60,code=sm_60 \
            -gencode arch=compute_61,code=sm_61"
 # Build NMS
-cd nms/src/cuda
+cd "nms/src/cuda"
 echo "Compiling nms kernels by nvcc..."
 /usr/local/cuda/bin/nvcc -c -o nms_kernel.cu.o nms_kernel.cu -x cu -Xcompiler -fPIC $CUDA_ARCH
 cd ../../
